4e3014789e330c8e19b52a14e04e9e06f25350a9
cbkim <cbkim@artylab.co.kr>
2022-03-13 23:56:04 +0900

    해시태그 테스트  - url 지정  - 액티비티 이동(데이터전달)

--- app/src/main/java/com/example/hashtag/HashTagActivity.java
+++ app/src/main/java/com/example/hashtag/HashTagActivity.java
@@ -2,7 +2,11 @@ package com.example.hashtag;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class HashTagActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class HashTagActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hash_tag);
+        TextView txt = findViewById(R.id.textView);
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+            Log.d("태그테스트", "ACTION : " + intent.getAction());
+
+            Uri uri = intent.getData();
+            Log.d("태그테스트", "uri : " + uri);
+            String srt = uri.toString();
+            String tag = srt.substring(srt.indexOf("#")+1,srt.length());
+            Log.d("태그테스트", "tag : " + tag);
+
+            String temp_post_id = uri.getQueryParameter("hashtag");
+            Log.d("태그테스트", "태그 : " + temp_post_id);
+
+            txt.setText(tag);
+        }
+
     }
 }
--- app/src/main/java/com/example/hashtag/MainActivity.java
+++ app/src/main/java/com/example/hashtag/MainActivity.java
@@ -10,6 +10,8 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     Linkify.TransformFilter mFilter;
 
     String[] srt;
+    String url ="hash://detail";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +36,28 @@ public class MainActivity extends AppCompatActivity {
         res = findViewById(R.id.editResult);
         btn = findViewById(R.id.button);
 
-        setEvent();
+        String content = req.getText().toString();
 
+        mFilter = new Linkify.TransformFilter() {
+            @Override
+            public String transformUrl(Matcher matcher, String s) {
+                System.out.println("태그테스트 " + url+"?hashTag="+s);
+                return "";
+            }
+        };
+
+        Pattern MY_PATTERN = Pattern.compile("#(\\S+)");
+        Matcher mat = MY_PATTERN.matcher(content);
+        List<String> tags = new ArrayList<>();
+        while (mat.find()) {
+            String tag = "#"+mat.group(1);
+            pattern1 = Pattern.compile(tag);
+            System.out.println("태그테스트 " + tag);
+            Linkify.addLinks(req, pattern1, url+"?hashTag="+tag, null, mFilter);
+        }
+    }
+
+    private void hashTagTest() {
         srt = new String[4];
         srt[0] = "#해시태그";
         srt[1] = "#해시";
@@ -44,27 +67,18 @@ public class MainActivity extends AppCompatActivity {
         mFilter = new Linkify.TransformFilter() {
             @Override
             public String transformUrl(Matcher matcher, String s) {
-                return "";
+                return url+"s";
             }
         };
-
-
-
 
         for( int i = 0; i<srt.length; i++ ) {
             Log.d("tag", "--> " + srt[i]);
             pattern1 = Pattern.compile(srt[i]);
-            String url ="hash://detail";
+
             Linkify.addLinks(req, pattern1, url, null, mFilter);
             //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
             //startActivity(intent);
         }
-    }
-
-    private void setEvent() {
-        btn.setOnClickListener(view -> {
-
-        });
     }
 
 }
